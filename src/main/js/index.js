4e8e09d8be811a470a7da9d22b73192d31166be7
tfennelly <tom.fennelly@gmail.com>
2016-05-11 08:59:13 +0100

    Do not initialize SSE client if there's no window
    e.g. in a test

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,17 +1,38 @@
-var internal = require('./sse-client');
+var eventSourceSupported = undefined;
 
-internal.connect();
+try {
+    eventSourceSupported = (window !== undefined && window.EventSource !== undefined);
+} catch (e) {}
 
-/**
- * Subscribe to a channel.
- * @param channelName The channel name.
- * @param filter An optional filter for the channel events.
- * @param callback The callback to be called on receipt of these events.
- */
-exports.subscribe = internal.subscribe;
+if (eventSourceSupported) {
+    var internal = require('./sse-client');
+    
+    internal.connect();
+    
+    /**
+     * Subscribe to a channel.
+     * @param channelName The channel name.
+     * @param filter An optional filter for the channel events.
+     * @param callback The callback to be called on receipt of these events.
+     */
+    exports.subscribe = internal.subscribe;
+    
+    /**
+     * Subscribe from a channel.
+     * @param callback The callback used when subscribing.
+     */
+    exports.unsubscribe = internal.unsubscribe;
+} else {
+    function noEventSource() {
+        console.warn("Jenskins SSE Gateway Client: This browser does not support EventSource. Where's the polyfill?");
+        // TODO: Need to add browser poly-fills for stuff like this
+        // See https://github.com/remy/polyfills/blob/master/EventSource.js
+    }
 
-/**
- * Subscribe from a channel.
- * @param callback The callback used when subscribing.
- */
-exports.unsubscribe = internal.unsubscribe;
+    exports.subscribe = function() {
+        noEventSource();
+    };
+    exports.unsubscribe = function() {
+        noEventSource();
+    };
+}
