7ec7721ac381bcc5c9e4553006829a891fc51e3d
tfennelly <tom.fennelly@gmail.com>
2016-05-11 10:32:45 +0100

    Better handling of errors relating to an unexpected Jenkins configuration

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,13 +2,22 @@ var eventSourceSupported = undefined;
 
 try {
     eventSourceSupported = (window !== undefined && window.EventSource !== undefined);
-} catch (e) {}
+} catch (e) {
+    // No window. Probably running in a test.
+}
+
+function noEventSource() {
+    console.warn('Jenskins SSE Gateway Client: This browser does not support EventSource. ' +
+        "Where's the polyfill?");
+    // TODO: Need to add browser poly-fills for stuff like this
+    // See https://github.com/remy/polyfills/blob/master/EventSource.js
+}
 
 if (eventSourceSupported) {
     var internal = require('./sse-client');
-    
+
     internal.connect();
-    
+
     /**
      * Subscribe to a channel.
      * @param channelName The channel name.
@@ -16,23 +25,17 @@ if (eventSourceSupported) {
      * @param callback The callback to be called on receipt of these events.
      */
     exports.subscribe = internal.subscribe;
-    
+
     /**
      * Subscribe from a channel.
      * @param callback The callback used when subscribing.
      */
     exports.unsubscribe = internal.unsubscribe;
 } else {
-    function noEventSource() {
-        console.warn("Jenskins SSE Gateway Client: This browser does not support EventSource. Where's the polyfill?");
-        // TODO: Need to add browser poly-fills for stuff like this
-        // See https://github.com/remy/polyfills/blob/master/EventSource.js
-    }
-
-    exports.subscribe = function() {
+    exports.subscribe = function () {
         noEventSource();
     };
-    exports.unsubscribe = function() {
+    exports.unsubscribe = function () {
         noEventSource();
     };
 }
