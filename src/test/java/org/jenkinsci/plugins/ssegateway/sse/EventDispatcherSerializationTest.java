ea74b1b210e29714aab8b72b491e33d1a268b070
tfennelly <tom.fennelly@gmail.com>
2017-01-13 18:53:31 +0000

    Added EventDispatcherSerializationTest

---
+++ src/test/java/org/jenkinsci/plugins/ssegateway/sse/EventDispatcherSerializationTest.java
@@ -0,0 +1,60 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jenkinsci.plugins.ssegateway.sse;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ * See https://issues.jenkins-ci.org/browse/JENKINS-41063
+ * <p>
+ * Yes there are probably other more thorough tests we could write,
+ * but I think these are enough.
+ *
+ * @author <a href="mailto:tom.fennelly@gmail.com">tom.fennelly@gmail.com</a>
+ */
+public class EventDispatcherSerializationTest {
+
+    @Test
+    public void test_AsynchEventDispatcher() throws IOException {
+        serialize(new AsynchEventDispatcher());
+    }
+
+    @Test
+    public void test_SynchEventDispatcher() throws IOException {
+        serialize(new SynchEventDispatcher());
+    }
+
+    private void serialize(EventDispatcher dispatcher) throws IOException {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+             ObjectOutputStream out = new ObjectOutputStream(outputStream)) {
+
+            out.writeObject(dispatcher);
+            out.flush();
+        }
+    }
+}
