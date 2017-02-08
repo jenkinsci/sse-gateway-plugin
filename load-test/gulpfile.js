af76d64a217f28a4758c1872de42637d250f5fa6
tfennelly <tom.fennelly@gmail.com>
2017-02-08 11:14:19 +0000

    rename scripts based on target tests

--- load-test/gulpfile.js
+++ load-test/gulpfile.js
@@ -4,4 +4,4 @@ var builder = require('@jenkins-cd/js-builder');
 // Bundle the modules.
 // See https://github.com/jenkinsci/js-builder
 //
-builder.bundle('src/main/js/sse-gateway-load.js').less('src/main/js/sse-gateway-load.less');
+builder.bundle('src/main/js/sse-gateway-browser-load.js').less('src/main/js/sse-gateway-browser-load.less');
