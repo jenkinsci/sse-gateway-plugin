3f3bdc19f28c852301703ad0e127ac6742ad93c8
tfennelly <tom.fennelly@gmail.com>
2017-02-09 21:03:46 +0000

    browser script using Harness

--- load-test/src/main/js/Harness.js
+++ load-test/src/main/js/Harness.js
@@ -28,34 +28,34 @@ Harness.prototype = {
                 runResults.push(result);
                 if (runResults.length === numChannels) {
                     // all channels are done now
+                    var startTimeMillis = runResults[0].startTimeMillis;
+                    var endTimeMillis = runResults[0].endTimeMillis;
+                    var failureCount = 0;
+                    for (var i = 1; i < runResults.length; i++) {
+                        startTimeMillis = Math.min(startTimeMillis, runResults[i].startTimeMillis);
+                        endTimeMillis = Math.max(endTimeMillis, runResults[i].endTimeMillis);
+                        failureCount += runResults[i].failureCount;
+                    }
+                    var timeTaken = endTimeMillis - startTimeMillis;
+                    var totalMessages = (numMessages * numChannels);
+                    var timePerMessage = timeTaken/totalMessages;
+                    var timePerMessageMinusInterval = (timePerMessage - intervalMillis);
+
+                    // Log the results
+                    if (failureCount === 0) {
+                        self.loggerFunc();
+                        self.loggerFunc('Yipee ... all ' + totalMessages + ' event messages received (' + numMessages + ' * ' + numChannels + ' channels) !!');
+                    } else {
+                        self.loggerFunc('Grrrr ... all ' + totalMessages + ' event messages NOT received (' + numMessages + ' * ' + numChannels + ' channels). Failure count ' + failureCount);
+                    }
+                    self.loggerFunc("Time taken: " + timeTaken + "ms, with a " + intervalMillis + "ms sleep between each message.");
+                    if (numChannels === 1) {
+                        self.loggerFunc("   That's " + timePerMessage + "ms per message (or " + timePerMessageMinusInterval + "ms if the sleep is factored out).");
+                    } else {
+                        self.loggerFunc("   That's " + timePerMessage + "ms per message.");
+                    }
+
                     if (onDone) {
-                        var startTimeMillis = runResults[0].startTimeMillis;
-                        var endTimeMillis = runResults[0].endTimeMillis;
-                        var failureCount = 0;
-                        for (var i = 1; i < runResults.length; i++) {
-                            startTimeMillis = Math.min(startTimeMillis, runResults[i].startTimeMillis);
-                            endTimeMillis = Math.max(endTimeMillis, runResults[i].endTimeMillis);
-                            failureCount += runResults[i].failureCount;
-                        }
-                        var timeTaken = endTimeMillis - startTimeMillis;
-                        var totalMessages = (numMessages * numChannels);
-                        var timePerMessage = timeTaken/totalMessages;
-                        var timePerMessageMinusInterval = (timePerMessage - intervalMillis);
-
-                        // Log the results
-                        if (failureCount === 0) {
-                            self.loggerFunc();
-                            self.loggerFunc('Yipee ... all ' + totalMessages + ' event messages received (' + numMessages + ' * ' + numChannels + ' channels) !!');
-                        } else {
-                            self.loggerFunc('Grrrr ... all ' + totalMessages + ' event messages NOT received (' + numMessages + ' * ' + numChannels + ' channels). Failure count ' + failureCount);
-                        }
-                        self.loggerFunc("Time taken: " + timeTaken + "ms, with a " + intervalMillis + "ms sleep between each message.");
-                        if (numChannels === 1) {
-                            self.loggerFunc("   That's " + timePerMessage + "ms per message (or " + timePerMessageMinusInterval + "ms if the sleep is factored out).");
-                        } else {
-                            self.loggerFunc("   That's " + timePerMessage + "ms per message.");
-                        }
-
                         onDone();
                     }
                 }
--- load-test/src/main/js/sse-gateway-browser-load.js
+++ load-test/src/main/js/sse-gateway-browser-load.js
@@ -4,59 +4,41 @@ $(document).ready(function start() {
     // See https://github.com/jenkinsci/sse-gateway-plugin
     var sse = require('@jenkins-cd/sse-gateway');
     var connection = sse.connect('sse-gateway-load');
+    var Harness = require('./Harness');
 
     var numMessagesInput = $('#numMessages');
     var intervalMillisInput = $('#intervalMillis');
-    var numMessages;
-    var intervalMillis;
+    var numChannelsInput = $('#numChannels');
     var logWindow = $('#event-logs');
-    var messageReceivedCount = 0;
-    var startTimeMillis;
-    var endTimeMillis;
+    var loggerPre = $('<pre>');
+
+    var triggerFunc = function(channelName, numMessages, intervalMillis) {
+        $.get('./fireEvents', {
+            channelName: channelName,
+            numMessages: numMessages,
+            intervalMillis: intervalMillis
+        });
+    };
+    var loggerFunc = function(message) {
+        loggerPre.append((message?message:'') + '\n');
+    };
+    function reset() {
+        logWindow.empty();
+        loggerPre.empty();
+        logWindow.append(loggerPre);
+    }
+    reset();
 
     $('#runButton').click(function () {
-        numMessages = parseInt(numMessagesInput.val());
-        intervalMillis = parseInt(intervalMillisInput.val());
+        var numMessages = parseInt(numMessagesInput.val());
+        var intervalMillis = parseInt(intervalMillisInput.val());
+        var numChannels = parseInt(numChannelsInput.val());
 
-        if ($('.event-log', logWindow).length === 0) {
-            logWindow.empty();
-            for (var i = 0; i < numMessages; i++) {
-                var logEvent = $('<div class="event-log">');
-                logEvent.attr('id', 'event-log-' + i);
-                logEvent.text(i);
-                logWindow.append(logEvent);
-            }
-
-            messageReceivedCount = 0;
-            $.get('./fireEvents', {
-                numMessages: numMessages,
-                intervalMillis: intervalMillisInput.val()
-            });
-        }
+        var harness = new Harness(connection, triggerFunc, loggerFunc);
+        harness.run(numMessages, intervalMillis, numChannels);
     });
+
     $('#resetButton').click(function () {
-        logWindow.empty();
-    });
-
-    connection.subscribe('load-test', function (event) {
-        $('#event-log-' + event.eventId).remove();
-
-        if (messageReceivedCount === 0) {
-            startTimeMillis = Date.now();
-        }
-        messageReceivedCount++;
-        if (messageReceivedCount === numMessages) {
-            endTimeMillis = Date.now();
-            if ($('.event-log', logWindow).length === 0) {
-                var timeTaken = endTimeMillis - startTimeMillis;
-                var timePerMessage = timeTaken/numMessages;
-                var timePerMessageMinusInterval = (timePerMessage - intervalMillis);
-
-                logWindow.append("<div>Time taken: " + timeTaken + "ms for " + numMessages + " messages, with a " + intervalMillis + "ms sleep between each message.</div>");
-                logWindow.append("<div>That's " + timePerMessage + "ms per message (or " + timePerMessageMinusInterval + "ms if the sleep is factored out).</div>");
-                logWindow.append("<p/>");
-                logWindow.append("<div>Also remember that some of the overall time may (may not - just keep it in mind) relate to client side DOM manipulation (looking for and removing the little message indicators). The CLI node load test may be a more accurate timing test.</div>");
-            }
-        }
+        reset();
     });
 });
