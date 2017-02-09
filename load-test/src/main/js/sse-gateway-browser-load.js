e79e1f04170653639e6c14c537a9303435c0b2c0
tfennelly <tom.fennelly@gmail.com>
2017-02-09 13:22:07 +0000

    added timing to the browser test script

--- load-test/src/main/js/sse-gateway-browser-load.js
+++ load-test/src/main/js/sse-gateway-browser-load.js
@@ -7,12 +7,19 @@ $(document).ready(function start() {
 
     var numMessagesInput = $('#numMessages');
     var intervalMillisInput = $('#intervalMillis');
+    var numMessages;
+    var intervalMillis;
     var logWindow = $('#event-logs');
+    var messageReceivedCount = 0;
+    var startTimeMillis;
+    var endTimeMillis;
 
     $('#runButton').click(function () {
-        if ($('.event-log', logWindow).length === 0) {
-            var numMessages = parseInt(numMessagesInput.val());
+        numMessages = parseInt(numMessagesInput.val());
+        intervalMillis = parseInt(intervalMillisInput.val());
 
+        if ($('.event-log', logWindow).length === 0) {
+            logWindow.empty();
             for (var i = 0; i < numMessages; i++) {
                 var logEvent = $('<div class="event-log">');
                 logEvent.attr('id', 'event-log-' + i);
@@ -20,6 +27,7 @@ $(document).ready(function start() {
                 logWindow.append(logEvent);
             }
 
+            messageReceivedCount = 0;
             $.get('./fireEvents', {
                 numMessages: numMessages,
                 intervalMillis: intervalMillisInput.val()
@@ -32,5 +40,23 @@ $(document).ready(function start() {
 
     connection.subscribe('load-test', function (event) {
         $('#event-log-' + event.eventId).remove();
+
+        if (messageReceivedCount === 0) {
+            startTimeMillis = Date.now();
+        }
+        messageReceivedCount++;
+        if (messageReceivedCount === numMessages) {
+            endTimeMillis = Date.now();
+            if ($('.event-log', logWindow).length === 0) {
+                var timeTaken = endTimeMillis - startTimeMillis;
+                var timePerMessage = timeTaken/numMessages;
+                var timePerMessageMinusInterval = (timePerMessage - intervalMillis);
+
+                logWindow.append("<div>Time taken: " + timeTaken + "ms for " + numMessages + " messages, with a " + intervalMillis + "ms sleep between each message.</div>");
+                logWindow.append("<div>That's " + timePerMessage + "ms per message (or " + timePerMessageMinusInterval + "ms if the sleep is factored out).</div>");
+                logWindow.append("<p/>");
+                logWindow.append("<div>Also remember that a some of the overall time may (may not - just keep it in mind) relate to client side dom manipulation (looking for and removing the little message indicators). The CLI node load test would be a more accurate timing test.</div>");
+            }
+        }
     });
 });
